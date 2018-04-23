3c08d0851346d14d6c123f19a1b90a7b32db36f7
Anu Khambete <anukhambete@gmail.com>
2018-04-23 18:42:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,9 @@ class Song
     self.class.all << self
   end
   
-  def self.create(song)
-    
-    
+  def self.create(name)
+    song = self.new
+    song.name = name
+    @@all << song
   end
 end
